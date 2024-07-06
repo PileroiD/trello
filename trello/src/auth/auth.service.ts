d00878655d3cd4cc0d8e3e01c4a5a09e1d58b4ef
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-07-05 20:57:41 -0400

    auth is completed

---
+++ trello/src/auth/auth.service.ts
@@ -0,0 +1,116 @@
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+    UnauthorizedException,
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { UserService } from "src/user/user.service";
+import { AuthDto } from "./dto/auth.dto";
+import { verify } from "argon2";
+import { Response } from "express";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class AuthService {
+    readonly REFRESH_TOKEN_NAME = "refreshToken";
+    readonly EXPIRE_DAY_REFRESH_TOKEN = 1;
+
+    constructor(
+        private jwt: JwtService,
+        private userService: UserService,
+        private readonly configService: ConfigService
+    ) {}
+
+    async login(dto: AuthDto) {
+        const { password, ...user } = await this.validateUser(dto);
+        const tokens = this.issueTokens(user.id);
+
+        return {
+            user,
+            ...tokens,
+        };
+    }
+
+    async register(dto: AuthDto) {
+        const oldUser = await this.userService.getByEmail(dto.email);
+
+        if (oldUser) throw new BadRequestException("User already exists");
+
+        const { password, ...user } = await this.userService.create(dto);
+
+        const tokens = this.issueTokens(user.id);
+
+        return {
+            user,
+            ...tokens,
+        };
+    }
+
+    async getNewTokens(refreshToken: string) {
+        const result = await this.jwt.verifyAsync(refreshToken);
+        if (!result) throw new UnauthorizedException("Invalid refresh token");
+
+        const { password, ...user } = await this.userService.getById(result.id);
+
+        const tokens = this.issueTokens(user.id);
+
+        return {
+            user,
+            ...tokens,
+        };
+    }
+
+    private issueTokens(userId: string) {
+        const data = { id: userId };
+
+        const accessToken = this.jwt.sign(data, {
+            expiresIn: "1h",
+        });
+
+        const refreshToken = this.jwt.sign(data, {
+            expiresIn: "7d",
+        });
+
+        return { accessToken, refreshToken };
+    }
+
+    private async validateUser(dto: AuthDto) {
+        const user = await this.userService.getByEmail(dto.email);
+
+        if (!user) throw new NotFoundException("User not found");
+
+        const isValid = await verify(user.password, dto.password);
+
+        if (!isValid) throw new UnauthorizedException("Invalid password");
+
+        return user;
+    }
+
+    addRefreshTokenToResponse(res: Response, refreshToken: string) {
+        const domain = this.configService.get<string>("DOMAIN");
+
+        const expiresIn = new Date();
+        expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+        res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+            httpOnly: true,
+            domain,
+            expires: expiresIn,
+            secure: true,
+            sameSite: "none",
+        });
+    }
+
+    removeRefreshTokenFromResponse(res: Response) {
+        const domain = this.configService.get<string>("DOMAIN");
+
+        res.cookie(this.REFRESH_TOKEN_NAME, "", {
+            httpOnly: true,
+            domain,
+            expires: new Date(0),
+            secure: true,
+            sameSite: "none",
+        });
+    }
+}
--- trello/src/main.ts
+++ trello/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(4444);
+
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>("PORT");
+
+    app.setGlobalPrefix("api");
+    app.use(cookieParser());
+    app.enableCors({
+        origin: true,
+        credentials: true,
+        exposedHeaders: "set-cookie",
+    });
+
+    await app.listen(port);
 }
 bootstrap();
