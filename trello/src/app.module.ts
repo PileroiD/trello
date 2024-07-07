b66de7bb60fc6e05bf2c26401f4eecd6727a858e
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-07-06 20:22:37 -0400

    time-blocks are completed

--- trello/src/app.module.ts
+++ trello/src/app.module.ts
@@ -4,8 +4,15 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { TaskModule } from "./task/task.module";
+import { TimeBlockModule } from "./time-block/time-block.module";
 
 @Module({
-    imports: [AuthModule, UserModule, TaskModule, ConfigModule.forRoot()],
+    imports: [
+        AuthModule,
+        UserModule,
+        TaskModule,
+        TimeBlockModule,
+        ConfigModule.forRoot(),
+    ],
 })
 export class AppModule {}
