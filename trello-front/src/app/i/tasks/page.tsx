92ef81855f79f74ced87e5dc8b718cf8d2e4e08e
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-07-18 16:06:47 -0400

    tasks

---
+++ trello-front/src/app/i/tasks/page.tsx
@@ -0,0 +1,18 @@
+import { Heading } from "@/components/ui/Heading";
+import { TasksView } from "./TasksView";
+import { Metadata } from "next";
+import { NO_INDEX_PAGE } from "@/constants/seo.constants";
+
+export const metadata: Metadata = {
+    title: "Tasks",
+    ...NO_INDEX_PAGE,
+};
+
+export default function TasksPage() {
+    return (
+        <div>
+            <Heading title="Tasks" />
+            <TasksView />
+        </div>
+    );
+}
